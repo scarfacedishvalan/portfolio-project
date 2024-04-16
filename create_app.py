07f66947461c4d7c0f4a09d1bb8304c98408ae1b
scarfacedishvalan <abhirakshit@gmail.com>
2024-04-16 11:23:34 +0530

    heading to deploy

--- create_app.py
+++ create_app.py
@@ -17,7 +17,7 @@ def create_app():
 
     app.layout = html.Div([
         # represents the browser address bar and doesn't render anything
-        html.H1("Optimal Strategies for Portfolio Allocation"),
+        html.H1("Optimal Strategies for Portfolio Allocation", style={'textAlign': 'center'}),
         dcc.Location(id='url', refresh=False),
         html.Div(id='page-content', children = overall_bt_layout)
     ])
